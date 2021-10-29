0f2307ca9ab2a870c6c560164d7cb027182b7480
Michal Slesar <michal.slesar@protonmail.com>
2021-10-29 15:33:48 +0200

    Use persistent threads in color_switch example

--- shaders/color_switch.glsl
+++ shaders/color_switch.glsl
@@ -1,13 +1,54 @@
 ${engineShader}
 
-void main() {
-    const ivec3 position = ivec3(gl_GlobalInvocationID);
+layout(local_size_x = 4) in;
 
-    if(key_pressed(KEY_B)) {
-        imageStore(outputImage, position.xy, vec4(0.3f, 0.3f, 0.3f, 1.0f));
-    } else if(key_pressed(KEY_A)) {
-        imageStore(outputImage, position.xy, vec4(0.7f, 0.7f, 0.7f, 1.0f));
-    } else {
-        imageStore(outputImage, position.xy, vec4(0, 0, 0, 1.0f));
+shared uint done;
+
+void main() {
+    if(gl_LocalInvocationIndex == 0 && gl_NumWorkGroups == 1) {
+        globalWorkSize.x = 2;
+        globalWorkSize.y = 2;
+        globalWorkSize.z = 1;
+        return;
+    }
+
+    if(gl_LocalInvocationIndex == 0) {
+        done = 0;
+    }
+
+    barrier();
+
+    while(done < 256 * 256)
+    {
+        uint previous = atomicAdd(done, 1);
+
+        // Calculate image position
+        const uint x = previous % 256;
+        const uint y = previous / 256;
+        const ivec3 position = ivec3(gl_WorkGroupID * uvec3(256, 256, 1)) + ivec3(x, y, 0);
+
+        if(key_pressed(KEY_B)) {
+            imageStore(outputImage, position.xy, vec4(0.3f, 0.3f, 0.3f, 1.0f));
+        } else if(key_pressed(KEY_A)) {
+            imageStore(outputImage, position.xy, vec4(0.7f, 0.7f, 0.7f, 1.0f));
+        } else {
+            imageStore(outputImage, position.xy, vec4(0, 0, 0, 1.0f));
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
